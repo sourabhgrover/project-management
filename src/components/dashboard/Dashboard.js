ae125dd715d67296010592c457325b4b6ee50ad5
Sourabh Grover <sourabhgrover29@gmail.com>
2020-02-18 17:43:40 +0530

    Setup ProjectListing Component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,15 @@
 import React from "react";
+
 import Notifications from "./Notifications";
+import ProjectList from "../projects/ProjectList"
 
 const Dashboard = () => {
     return (
         <div className="dashboard container">
             <div className="row">
                 <div className="col s12 m6">
-                    Project List will come here
-            </div>
+                    <ProjectList />
+                </div>
                 <div className="col s12 m5 offset-m1">
                     <Notifications />
                 </div>
